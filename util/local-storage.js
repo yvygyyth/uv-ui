de662ea555a7988d40324d996dcd9f735ba1dec6
虞子楠 <12227713+yu-zinan@user.noreply.gitee.com>
2024-10-11 10:24:04 +0800

    feat：本地存储修改成uniapp

--- util/local-storage.js
+++ util/local-storage.js
@@ -1,31 +1,27 @@
 export default class WebStorage {
-    static getItem(key, config = {}) {
-        const storage = config.storage || window.localStorage;
-        const json = storage.getItem(key);
-        if (!json) return null;
-        const { value, expired } = JSON.parse(json);
-        if (expired && Date.now() >= expired) {
-          storage.removeItem(key);
-            return null;
-      }
-      return value;
+  static getItem(key) {
+    const json = uni.getStorageSync(key);
+    if (!json) return null;
+
+    const { value, expired } = json;
+    if (expired && Date.now() >= expired) {
+      uni.removeStorageSync(key);
+      return null;
     }
-  
-    static setItem(key, value, config = {}) {
-      const expired = config.expired || 0;
-      const storage = config.storage || window.localStorage;
-      const data = { value, expired };
-      storage.setItem(key, JSON.stringify(data));
-    }
-  
-    static removeItem(key, config = {}) {
-      const storage = config.storage || window.localStorage;
-      storage.removeItem(key);
-    }
-  
-    static clear(config = {}) {
-      const storage = config.storage || window.localStorage;
-      storage.clear();
-    }
+    return value;
+  }
+
+  static setItem(key, value, config = {}) {
+    const expired = config.expired || 0;
+    const data = { value, expired };
+    uni.setStorageSync(key, data);
+  }
+
+  static removeItem(key) {
+    uni.removeStorageSync(key);
+  }
+
+  static clear() {
+    uni.clearStorageSync();
+  }
 }
-  
